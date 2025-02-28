0f3db7d1c87eebd26a2717a25d43c45a3926b4a4
Sajjad <sajjadqaderi00@gmail.com>
2025-02-28 16:25:57 +0100

    Databas is ready to use

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,19 +1,35 @@
+import { date } from "drizzle-orm/mysql-core";
 import {
     integer, 
     pgTable, 
     varchar,
     uuid,
     pgEnum,
+    timestamp,
 } from "drizzle-orm/pg-core";
 
 // skapat en enum 
 export const roleEnum = pgEnum('role', ['User', 'Admin']);
+export const roomEnum =pgEnum('roomType', ['Workspace', 'Conference']);
 
 export const userSchema = pgTable('users', {
     id: uuid().defaultRandom().primaryKey(),
-    username: varchar({length:200}).unique().notNull(),
-    password: varchar({length:200}).notNull(),
+    username: varchar({ length:200 }).unique().notNull(),
+    password: varchar({ length:200 }).notNull(),
     role: roleEnum().default('User')
 });
 
+export const roomsSchema = pgTable('rooms', {
+    id: uuid().defaultRandom().primaryKey(),
+    name: varchar({ length: 200 }).notNull(),
+    capacity: integer().notNull(),
+    type: roomEnum().notNull()
+})
+
+export const bookingSchema = pgTable('bookings', {
+    roomID: uuid().references(() => roomsSchema.id).notNull(),
+    userID: uuid().references(() => userSchema.id).notNull(),
+    startTime: timestamp({ mode: 'date' }).notNull(),
+    endTime: timestamp({ mode: 'date' }).notNull()
+})
 
--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,10 @@ const port = process.env.SERVER_PORT;
 // middleware 
 app.use(express.json());
 
+app.get('/getData', (req, res) => {
+    res.send('Hello world!');
+})
+
 app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`)
 })
