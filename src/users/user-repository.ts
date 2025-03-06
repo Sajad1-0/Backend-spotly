251247dec1d615d4bd96c2970b060bf9cb269374
Sajjad <Sajadehsas10@gmail.com>
2025-03-06 20:46:05 +0100

    user Repository is done now

--- src/users/user-repository.ts
+++ src/users/user-repository.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import { userSchema } from '../db/schema';
 import { db } from '../rooms/rooms-Repository';
+import { eq, and } from 'drizzle-orm';
 
 // typen ska innehålla samma egenskaper som databasen. 
 export type CreateUser = typeof userSchema.$inferInsert;
@@ -14,5 +15,37 @@ export class userRepository {
         .values(createUser)
         .returning({insertedID: userSchema.id})
         return CreatedUser[0].insertedID;
+    }
+
+    async findAllUsers(): Promise<CreateUser[]> {
+        return await db.select()
+        .from(userSchema)
+    }
+
+    async findOneUser(id: string): Promise<CreateUser> {
+        const findUser = await db.select()
+        .from(userSchema)
+        .where(and(
+            eq(userSchema.id, id)
+        ))
+
+        if (findUser.length === 0) {
+            throw new Error ('User not been found!')
+        }
+
+        return findUser[0]
+    }
+
+    async update(id: string, updateUser: UpdateUser): Promise<string> {
+        const updateRooms = await db.update(userSchema)
+        .set(updateUser)
+        .where(eq(userSchema.id, id))
+        .returning({updateId: userSchema.id})
+
+        if (updateRooms.length === 0) {
+            throw new Error('User has not been found')
+        }
+
+        return updateRooms[0].updateId;
     }
 }
