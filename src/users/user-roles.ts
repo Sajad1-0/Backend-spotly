cbed2ef9e1d739515f37e4f1b3ee7ceb894805dd
Sajjad <sajjadqaderi00@gmail.com>
2025-03-26 13:24:39 +0100

    Roles har created and ready for implementation

---
+++ src/users/user-roles.ts
@@ -0,0 +1,29 @@
+enum Role {
+    Admin = 'Admin',
+    User = 'User'
+}
+
+
+export type RolesWithPermissions = {
+    [role in Role]: Permission[];
+};
+
+export type Permission = 'create' | 'read' | 'update' | 'delete';
+
+
+export const ROLES_WITH_PERMISSIONS: RolesWithPermissions = {
+    [Role.Admin] : ['create', 'read', 'update', 'delete'],
+    [Role.User] : ['read']
+}
+
+export type Roles = {
+    [role in Role]: Role[]
+}
+
+export const ROLE_HIERARCHY: Roles = {
+    [Role.Admin] : [Role.Admin], [Role.User],
+    [Role.User] : [Role.User]
+}
+
+
+
