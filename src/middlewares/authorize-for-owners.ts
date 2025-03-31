c1b2eaf489e951c641e19e64084097b6058d0c67
Sajjad <sajjadqaderi00@gmail.com>
2025-03-31 22:40:19 +0200

    Adding authriziation for Owners of bookings

---
+++ src/middlewares/authorize-for-owners.ts
@@ -0,0 +1,23 @@
+import { Request, Response, NextFunction } from "express";
+import { Role } from "../users/user-roles";
+import { httpCodeStatus } from "../httpStatus";
+import { AuthenticateRequest } from "../interfaces/booking-interface";
+import { BookingService } from "../bookings/booking-service";
+
+const bookingService = new BookingService();
+
+export const authorizeForOwners = async (req: AuthenticateRequest, res: Response, next: NextFunction) => {
+    
+    const bookingID = req.params.id;
+    const {userID: requesterUserID, role} = req.jwtPayload || {}
+
+    if(role === Role.Admin) next()
+
+    const {userID: ownerUserID} = await bookingService.findOne(bookingID);
+
+    if(requesterUserID === ownerUserID) next();
+
+    res.status(httpCodeStatus.NOT_AUTHORIZED)
+    .send(`User with this ${ownerUserID} hasn't rights to see information about this booking: ${bookingID}`)
+
+}
